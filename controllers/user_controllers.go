c7708508c309af9ecca42f0ce3f6f94cb7bf7512
fernode <fernandosilvadj@hotmail.com>
2024-06-01 19:57:47 -0300

    feat: add user controller

---
+++ controllers/user_controllers.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+	"github.com/fernode/goBookStore/models"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+func RegisterUserController(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
+	var user models.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+		return
+	}
+	user.Password = string(hashedPassword)
+
+	if err := db.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
+}
